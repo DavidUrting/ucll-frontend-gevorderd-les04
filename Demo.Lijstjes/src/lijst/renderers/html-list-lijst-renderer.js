9b4620c0943e4d1aae793284c1ffe80b5a634106
David Urting <david.urting@live.com>
2020-05-04 13:41:09 +0200

    stuyling

--- Demo.Lijstjes/src/lijst/renderers/html-list-lijst-renderer.js
+++ Demo.Lijstjes/src/lijst/renderers/html-list-lijst-renderer.js
@@ -24,20 +24,14 @@ export class HtmlListLijstRenderer extends LijstRenderer {
             let li = document.createElement("li");
             li.id = `${this.ulElement.id}-${o.id}`
             li.setAttribute("data-onderdeel-id", o.id);           
-            li.innerHTML = `<input type='text' readonly /><button>X</button>`;
+            li.innerHTML = `<div class='w-50 d-inline-block'></div><button>X</button>`;
             this.ulElement.appendChild(li);
             if (this.lijst instanceof SelecteerbareLijst) {
                 document.querySelector(`#${li.id}`).addEventListener("click", (e) => {
                     this.lijst.selectedOnderdeelId = o.id;
                 });
             }
-            document.querySelector(`#${li.id} input`).value = o.titel;
-            document.querySelector(`#${li.id} input`).addEventListener("input", (e) => {
-                this.skipEvents = true;
-                o.tekst = e.target.value;
-                this.skipEvents = false;
-                e.preventDefault();
-            });
+            document.querySelector(`#${li.id} div`).innerHTML = o.titel;
             document.querySelector(`#${li.id} button`).addEventListener("click", (e) => {
                 this.lijst.remove(o.id);
                 e.preventDefault();
